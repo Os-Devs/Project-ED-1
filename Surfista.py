c0b1c7bc3369660d186deddddd2052bcaaa1a488
John Marques <62351565+John-Fry@users.noreply.github.com>
2021-03-03 18:27:07 -0300

    Add Python Files

---
+++ Surfista.py
@@ -0,0 +1,32 @@
+from Prancha import Prancha
+from Campeonato import Campeonato
+
+class Surfista:
+	def __init__(self, nome, idade, campeonatos = [], pranchas = []):
+		self._nome = str(nome)
+		self._idade = int(idade)
+		self._campeonatos = campeonatos
+		self._pranchas = pranchas
+
+	@property
+	def nome_surfista(self):
+		return self._nome
+
+	@nome_surfista.setter
+	def nome_surfista(self, nome_surfista):
+		self._nome = nome_surfista
+
+	@property
+	def idade_surfista(self):
+		return self._idade
+
+	@idade_surfista.setter
+	def idade_surfista(self, idade_surfista):
+		self._idade = idade_surfista
+
+	def surfista_camps(self):
+		return self._campeonatos
+
+	def prancha_surfista(self):
+		return self._pranchas
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from Campeonato import Campeonato
+from Pais import Pais
+from Praia import Praias
+from Prancha import Prancha
+from Surfista import Surfista
+
+if __name__ == '__main__':
+    p1= Pais('Brazil','PT-BR', ['CaboBranco', 'Lucena','Bessa'])
+    print(p1)
